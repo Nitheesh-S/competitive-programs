e62fca18a719c6acd07b6be3e25adcd3fa140189
Nitheesh <nitheeshmsk@gmail.com>
2023-08-13 11:33:43 +0530

    Time: 1622 ms (88.44%), Space: 107.3 MB (88.17%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
@@ -1,14 +1,33 @@
+# print(edge, (a, b))
+# print(self.tree, self.size)
+
+# [0, 1] (0, 1)
+# [0, 0, 2, 3, 4, 5] [2, 1, 1, 1, 1, 1]
+# [0, 2] (0, 2)
+# [0, 0, 0, 3, 4, 5] [3, 1, 1, 1, 1, 1]
+# [3, 4] (3, 4)
+# [0, 0, 0, 3, 3, 5] [3, 1, 1, 2, 1, 1]
+# [3, 2] (3, 0)
+# [0, 0, 0, 0, 3, 5] [5, 1, 1, 2, 1, 1]
+
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-            self.tree = [i for i in range(n)]
+        self.tree = [i for i in range(n)]
+        self.size = [1] * n
 
-            for edge in edges:
-                start = self.root(edge[0])
-                end = self.root(edge[1])
-
-                self.tree[start] = end
-            
-            return self.root(source) == self.root(destination)
+        for edge in edges:
+            a = self.root(edge[0])
+            b = self.root(edge[1])
+            if a == b:
+                continue
+            if self.size[a] < self.size[b]:
+                self.tree[a] = b
+                self.size[b] += self.size[a]
+            else:
+                self.tree[b] = a
+                self.size[a] += self.size[b]
+        
+        return self.root(source) == self.root(destination)
     
     def root(self, node):
         while self.tree[node] != node:
