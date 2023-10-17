62e38f4057611d34cb99173a1bfa5cb9b5175d1d
Nitheesh <nitheeshmsk@gmail.com>
2023-10-18 01:02:01 +0530

    Time: 51 ms (72.85%), Space: 16.7 MB (73.77%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        store = {}
+        for c in s:
+            try:
+                store[c] += 1
+            except KeyError:
+                store[c] = 1
+        
+        for c in t:
+            try:
+                store[c] -= 1
+            except KeyError:
+                return False
+            
+            if store[c] == 0:
+                del store[c]
+        
+        return not bool(store)
